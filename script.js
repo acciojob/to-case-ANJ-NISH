467d15088d27af2c430f621e009ea3e50085a0c6
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-11-23 18:49:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 function toCase(text) {
-  // write your code here
+
+	if(text.length===0)
+	{
+		return "-";
+	}
+	let ans="";
+
+	for(let i=0;i<text.length;i++)
+		{
+		  if(text.charAt(i)===" ")
+		  {
+			  ans+=" ";
+			  continue;
+		  }
+			ans+= text.charAt(i).toLowerCase();
+		}
+	ans+="-";
+	for(let i=0;i<text.length;i++)
+		{
+		  if(text.charAt(i)===" ")
+		  {
+			  ans+=" ";
+			  continue;
+		  }
+			ans+= text.charAt(i).toUpperCase();
+		}
+	return ans;
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
-alert(toCase(text));
+// const text = prompt("Enter text:");
+// alert(toCase(text));
